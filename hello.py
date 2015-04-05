84a66d7382288b407909f4667c24e5cc658f6306
spravo13 <spencer@techandcode.com>
2015-04-04 23:40:22 -0700

    Add quest question and a randomized third question

--- hello.py
+++ hello.py
@@ -1,6 +1,20 @@
-select = input("What is your name? ")
+from random import randint
 
-print("Hello "+select+"!")
+name = input("What is your name? ")
 
-select = input("What is your quest? ")
+print("Hello "+name+"!")
+
+quest = input("What is your quest? ")
+
+rand_int = randint(1,3) #(Inclusive)
+
+if rand_int == 1:
+    color = input("What is your favorite color? ")
+    third_question = "color"
+elif rand_int == 2:
+    cap_of_assyria = input("What is the capitol of Assyria? ")
+    third_question = "assyria"
+elif rand_int == 3:
+    swallow = input("What is the airspeed velocity of an unladen swallow? ")
+    third_question = "swallow"
 
