194940849791e968f933d17888bbcc497a223ab6
spravo13 <spencer@techandcode.com>
2015-04-05 00:17:51 -0700

    Adjust first two questions, uniform quotations.
    Make first two questions so it doesn't store the responses in variables
    
    Change to all single quotes.

--- hello.py
+++ hello.py
@@ -1,32 +1,30 @@
 import random
 
-name = input("What is your name? ")
+print('What is your name? ')
 
-print("Hello "+name+"!")
-
-quest = input("What is your quest? ")
+print('What is your quest? ')
 
 rand_int = random.randint(0,3) #(Inclusive)
 
 if rand_int == 1:
     colorls = ['green', 'blue', 'yellow', 'red', 'orange', 'magenta', 'white', 'black', 'maroon', 'violet', 'gray', 'brown']
-    color = input("What is your favorite color? ")
+    color = input('What is your favorite color? ')
     if color in colorls:
-        print("Right. Off you go.")
+        print('Right. Off you go.')
     else:
-        print("*casts you into the abyss* Heh heh heh...")
+        print('*casts you into the abyss* Heh heh heh...')
 elif rand_int == 2:
     capls = ['Ashur', 'Nimrud', 'Dur Sarukin', 'Nineveh']
-    cap_of_assyria = input("What is the capitol of Assyria? ")
+    cap_of_assyria = input('What is the capitol of Assyria? ')
     if cap_of_assyria in capls:
-        print("Right. Off you go.")
+        print('Right. Off you go.')
     else:
-        print("*casts you into the abyss* Heh heh heh...")
+        print('*casts you into the abyss* Heh heh heh...')
 else:
-    swallow = input("What is the airspeed velocity of an unladen swallow?(in mph) ")
-    if "23" < swallow < "25":
-        print("Right. Off you go.")
-    elif swallow == "What do you mean? An African or European swallow?":
-        print("What? I don't know that! Auuuuuuuugh! *is cast into the abyss*")
+    swallow = input('What is the airspeed velocity of an unladen swallow?(in mph) ')
+    if '23' < swallow < '25':
+        print('Right. Off you go.')
+    elif swallow == 'What do you mean? An African or European swallow?':
+        print('What? I don\'t know that! Auuuuuuuugh! *is cast into the abyss*')
     else:
-        print("*casts you into the abyss* Heh heh heh...")
+        print('*casts you into the abyss* Heh heh heh...')
